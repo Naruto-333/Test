d0a5ac856642c07a1dc42ce75abd8b90e8169a2b
Naruto-333 <115845049+Naruto-333@users.noreply.github.com>
2026-02-20 21:41:46 -0600

    Add files via upload

---
+++ rbTourPreMove.py
@@ -0,0 +1,74 @@
+from machine import Pin
+import utime
+import time
+
+abort_time=22.73
+
+
+delta=0
+
+pins=[
+    Pin(15,Pin.OUT),#B-1A
+    Pin(17,Pin.OUT),#B-1B
+    Pin(16,Pin.OUT),#A-1A
+    Pin(14,Pin.OUT)#A-1B
+    ]
+
+dirs=[
+    [0,0,1,1], #forward
+    [1,1,0,0], #backward
+    [1,0,1,0], #turn left
+    [0,1,0,1],#turn right
+    [0,0,0,0] #null
+    ]
+
+start=time.time()
+
+
+
+def moveBot(direction, unit):
+    if direction == 'f':
+        pins[0].value(dirs[0][0])
+        pins[1].value(dirs[0][1])
+        pins[2].value(dirs[0][2])
+        pins[3].value(dirs[0][3])
+        utime.sleep(unit * (6/125))
+        pins[0].value(dirs[4][0])
+        pins[1].value(dirs[4][1])
+        pins[2].value(dirs[4][2])
+        pins[3].value(dirs[4][3])
+    if direction == 'b':
+        pins[0].value(dirs[1][0])
+        pins[1].value(dirs[1][1])
+        pins[2].value(dirs[1][2])
+        pins[3].value(dirs[1][3])
+        utime.sleep(unit * (6/125))
+        pins[0].value(dirs[4][0])
+        pins[1].value(dirs[4][1])
+        pins[2].value(dirs[4][2])
+        pins[3].value(dirs[4][3])
+    if direction == 'l':
+        pins[0].value(dirs[2][0])
+        pins[1].value(dirs[2][1])
+        pins[2].value(dirs[2][2])
+        pins[3].value(dirs[2][3])
+        utime.sleep(unit * (887/180000))
+        pins[0].value(dirs[4][0])
+        pins[1].value(dirs[4][1])
+        pins[2].value(dirs[4][2])
+        pins[3].value(dirs[4][3])
+    if direction == 'r':
+        pins[0].value(dirs[3][0])
+        pins[1].value(dirs[3][1])
+        pins[2].value(dirs[3][2])
+        pins[3].value(dirs[3][3])
+        utime.sleep(unit * (887/180000))
+        pins[0].value(dirs[4][0])
+        pins[1].value(dirs[4][1])
+        pins[2].value(dirs[4][2])
+        pins[3].value(dirs[4][3])
+        
+        
+while delta <= abort_time:
+    delta = time.time()-start
+    
